174fccdb357da32955654ced49492d378053cdd4
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-03-26 15:40:23 +0800

    提交代码

--- actions/outer/commit/LoginAction.php
+++ actions/outer/commit/LoginAction.php
@@ -7,23 +7,41 @@
  */
 
 namespace app\modules\actions\outer\commit;
+use app\modules\apis\WeiXinApi;
 use app\modules\services\outer\commit\LoginService;
 use sp_framework\actions\BaseAction;
 use sp_framework\components\Assert;
+use sp_framework\util\WxEncryptedDecode;
 
 class LoginAction extends BaseAction
 {
     private $code;
+    private $userData;
 
     protected function formatParams()
     {
-        $this->code = $this->get('code');
+        $this->code     = $this->get('code');
+        $this->userData = json_decode($this->get('userData'), true);
         Assert::isTrue(!empty($this->code), "网络繁忙,请稍后再试", "code不能为空");
+        Assert::isTrue(!empty($this->userData), "网络繁忙,请稍后再试", "userData不能为空");
     }
 
     public function execute()
     {
-        $loginResult = LoginService::login($this->code);
+        $response       = WeiXinApi::getSession($this->code)->toArray();
+        $wxSession      = $response['session_key'];
+
+        $sha1Content    = $this->userData['rawData'] . $wxSession;
+        $sign           = sha1($sha1Content);
+        Assert::isTrue($sign === $this->userData['signature'], "网络繁忙,请稍后再试", "微信数据签名验证失败");
+
+        $encryptedData  = [];
+        $decode         = new WxEncryptedDecode(WeiXinApi::APP_ID, $wxSession);
+        $decodeCode     = $decode->decryptData($this->userData['encryptedData'], $this->userData['iv'], $encryptedData);
+        Assert::isTrue($decodeCode === 0, "网络繁忙,请稍后再试", "解析用户数据失败,errorCode:{$decodeCode}");
+
+        $encryptedData  = json_decode($encryptedData, true);
+        $loginResult    = LoginService::login($this->code, $encryptedData['unionId']);
         return $loginResult;
     }
 }
--- services/outer/commit/LoginService.php
+++ services/outer/commit/LoginService.php
@@ -16,11 +16,9 @@ use sp_framework\util\RedisUtil;
 
 class LoginService
 {
-    public static function login($code){
+    public static function login($code, $unionID){
         $response       = WeiXinApi::getSession($code)->toArray();
         $wxSession      = $response['session_key'];
-        $unionID        = $response['unionid'];
-
         $userBean       = PassportUserModel::queryUserByWxUnionID($unionID);
         $memberID       = $userBean->getUuid();
         Assert::isTrue(!empty($userID), "登陆失败,请重试操作", "获取用户信息失败", 403);
