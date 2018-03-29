3997ef798d0fdbc2394862f4ee8faed575254a87
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-03-29 17:27:38 +0800

    提交代码

--- actions/outer/commit/RegisterAction.php
+++ actions/outer/commit/RegisterAction.php
@@ -17,18 +17,20 @@ class RegisterAction extends BaseAction
 {
     private $code;
     private $userData;
+    private $registerFrom;
 
     protected function formatParams()
     {
-        $this->code     = $this->get('code');
-        $this->userData = json_decode($this->get('userData'), true);
+        $this->code         = $this->get('code');
+        $this->userData     = json_decode($this->get('userData'), true);
+        $this->registerFrom = json_decode($this->get('registerFrom'), 1);
         Assert::isTrue(!empty($this->code), "网络繁忙,请稍后再试", "code不能为空");
         Assert::isTrue(!empty($this->userData), "网络繁忙,请稍后再试", "userData不能为空");
     }
 
     public function execute()
     {
-        $response       = WeiXinApi::getSession($this->code)->toArray();
+        $response       = WeiXinApi::getSession($this->code, $this->registerFrom)->toArray();
         $wxSession      = $response['session_key'];
 
         $sha1Content    = $this->userData['rawData'] . $wxSession;
@@ -36,7 +38,7 @@ class RegisterAction extends BaseAction
         Assert::isTrue($sign === $this->userData['signature'], "网络繁忙,请稍后再试", "微信数据签名验证失败");
 
         $encryptedData  = [];
-        $decode         = new WxEncryptedDecode(WeiXinApi::APP_ID, $wxSession);
+        $decode         = new WxEncryptedDecode(WeiXinApi::$appIDMap[$this->registerFrom], $wxSession);
         $decodeCode     = $decode->decryptData($this->userData['encryptedData'], $this->userData['iv'], $encryptedData);
         Assert::isTrue($decodeCode === 0, "网络繁忙,请稍后再试", "解析用户数据失败,errorCode:{$decodeCode}");
 
--- apis/WeiXinApi.php
+++ apis/WeiXinApi.php
@@ -7,30 +7,40 @@
  */
 
 namespace app\modules\apis;
-
 use sp_framework\apis\ApiContext;
 
 class WeiXinApi
 {
-    const APP_ID            = "wx9df0d56749f4bddd";
-    const SECRET            = "4a775ed373f8c78cf063b108cf8ceedc";
-    const SESSION_TYPE      = "authorization_code";
-    const ACCESS_TOKEN_TYPE = "client_credential";
+    const TEACHER_APP_ID            = "wx9df0d56749f4bddd";
+    const TEACHER_SECRET            = "4a775ed373f8c78cf063b108cf8ceedc";
+    const PARENT_APP_ID             = "wx4fad55e1b1555858";
+    const PARENT_SECRET             = "4a775ed373f8c78cf063b108cf8ceedc";
+    const SESSION_TYPE              = "authorization_code";
+    const ACCESS_TOKEN_TYPE         = "client_credential";
 
-    public static function getSession($js_code){
+    public static $appIDMap = [
+        1   => 'wx9df0d56749f4bddd',    //教师端
+        2   => 'wx4fad55e1b1555858',    //家长端
+    ];
+    public static $secretMap = [
+        1   => '4a775ed373f8c78cf063b108cf8ceedc',    //教师端
+        2   => '6d2401cfcf88ad88be9add9aa75b0232',    //家长端
+    ];
+
+    public static function getSession($js_code, $registerFrom){
         $params = [
-            'appid'         => self::APP_ID,
-            'secret'        => self::SECRET,
+            'appid'         => self::$appIDMap[$registerFrom],
+            'secret'        => self::$secretMap[$registerFrom],
             'js_code'       => $js_code,
             'grant_type'    => self::SESSION_TYPE
         ];
         return ApiContext::post('weixin', 'getSession', $params)->throwWhenFailed();
     }
 
-    public static function getAccessToken(){
+    public static function getAccessToken($registerFrom = 1){
         $params = [
-            'appid'         => self::APP_ID,
-            'secret'        => self::SECRET,
+            'appid'         => self::$appIDMap[$registerFrom],
+            'secret'        => self::$secretMap[$registerFrom],
             'grant_type'    => self::ACCESS_TOKEN_TYPE
         ];
         return ApiContext::post('weixin', 'getAccessToken', $params);
